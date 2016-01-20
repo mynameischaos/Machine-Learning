eef6c591a48989910efbc1d41a664cc437d72d60
Huasong Zhong <zhonghsuestc@163.com>
2016-01-20 10:36:43 +0800

    KNN

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -1,6 +1,6 @@
 import operator
-
 from numpy import *
+from os import listdir
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -71,5 +71,42 @@ def classifyPerson():
                                   minValue) / ranges, normMat, datingLables, 3)
     print 'You will probably like this person: ', \
         resultList[classifierResult - 1]
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr(j))
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((1, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, \
+                                     trainingMat, hwLabels, 3)
+        print 'the classifier came back with: %d, the real answer is: %d ' \
+                            % (classifierResult, classNumStr)
+        if(classifierResult != classNumStr): errorCount += 1.0
+
+    print "\n the total number of errors is: %d" % errorCount
+    print "\n the total error rate is: %f" % (errorCount / mTest)
 
 
